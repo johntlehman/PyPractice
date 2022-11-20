00314e8a73e1d9952f171a7ea9bfc01b162b2b02
johntlehman <jtlehman@hey.com>
2022-11-19 22:06:41 -0800

    minor updates

--- average_a_file.py
+++ average_a_file.py
@@ -4,11 +4,17 @@
 infile = open("numbers.csv", 'r')
 sum = 0
 count = 0
+total = 0
 line = infile.readline()
 while line != "":
-    sum = sum + eval(line)
-    count += 1
+    for xStr in line.split(","):
+        sum = sum + eval(xStr)
+        count += 1
     line = infile.readline()
-    print('/nThe average of the numbers is', sum / count)
+    print(f"There were {count} numbers on the line and their average was {sum / count}")
+    total = total + sum
+    count = 0
+    sum = 0
 
+print(f'the sum of all numbers in the file is {total}')
 # main()
